61a6afaf22bbaca6bf241c2627421e2bb30680ff
Charlie <124851081+Charliee105@users.noreply.github.com>
2025-03-18 09:28:05 -0400

    Led script

--- _LED_Tester_Script/_LED_Tester_Script.ino
+++ _LED_Tester_Script/_LED_Tester_Script.ino
@@ -1,27 +1,29 @@
 #include <FastLED.h>
 
-#define DATA_PIN 7
-#define NUM_LEDS 10
+#define LED_PIN     6    // Pin connected to the LED strip data input
+#define NUM_LEDS    30   // Number of LEDs in the strip
+#define BRIGHTNESS  150  // Brightness (0-255)
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
 
-CRGB leds[NUM_LEDS];
+CRGB leds[NUM_LEDS];  // Array to hold LED colors
 
 void setup() {
-FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+    FastLED.setBrightness(BRIGHTNESS);
 }
 
 void loop() {
-// Set all LEDs to red
-fill_solid(leds, NUM_LEDS, CRGB::Red);
-FastLED.show();
-delay(1000);
+    rainbowCycle(10);  // Cycle through rainbow colors
+}
 
-// Set all LEDs to green
-fill_solid(leds, NUM_LEDS, CRGB::Green);
-FastLED.show();
-delay(1000);
-
-// Set all LEDs to blue
-fill_solid(leds, NUM_LEDS, CRGB::Blue);
-FastLED.show();
-delay(1000);
+// Function to cycle through rainbow colors
+void rainbowCycle(int wait) {
+    for (int j = 0; j < 255; j++) {
+        for (int i = 0; i < NUM_LEDS; i++) {
+            leds[i] = CHSV((i * 256 / NUM_LEDS + j) & 255, 255, BRIGHTNESS);
+        }
+        FastLED.show();
+        delay(wait);
+    }
 }
