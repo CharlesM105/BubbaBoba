5a2f947b8b24dfed08aa49e1259c7ac74bd1c1a7
Charlie <124851081+Charliee105@users.noreply.github.com>
2025-03-19 09:48:32 -0400

    Update _LED_Tester_Script.ino

--- _LED_Tester_Script/_LED_Tester_Script.ino
+++ _LED_Tester_Script/_LED_Tester_Script.ino
@@ -2,7 +2,7 @@
 
 #define PIN 2	 // input pin Neopixel is attached to
 
-#define NUMPIXELS      9// number of neopixels in strip
+#define NUMPIXELS      200// number of neopixels in strip
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
