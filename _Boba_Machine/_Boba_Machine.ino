80df5fe71ce0e7e60f7539e47b3fb339a7d67800
Charlie <124851081+Charliee105@users.noreply.github.com>
2025-03-20 09:50:26 -0400

    Update _Boba_Machine.ino

--- _Boba_Machine/_Boba_Machine.ino
+++ _Boba_Machine/_Boba_Machine.ino
@@ -1,314 +1,265 @@
-//Yummy Boba Code
+// Yummy Boba + Menu System Integration
 
-
-//libaries
+// Libraries
 #include <AccelStepper.h>
 #include <Servo.h>
 #include <LiquidCrystal_I2C.h>
-//RFID Code Portion
 #include <SPI.h>
 #include <MFRC522.h>
 
-//variables
-int Homing1 = 0;
-int Homing2 = 0;
-int Homing3 = 0;
+// LCD Display
+#define I2C_ADDR 0x27
+#define LCD_COLUMNS 20
+#define LCD_LINES 4
+LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
+
+// RFID Module
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);  
+
+// LED
+#define LED_PIN 7
+
+// Rotary Encoder Pins
+#define ENCODER_CLK 49  
+#define ENCODER_DT 51  
+#define ENCODER_SW 53  
+
+// Rotary Encoder Variables
+volatile int menuIndex = 0;  
+int lastStateCLK;  
+bool buttonPressed = false;  
+int menuStartIndex = 0;  
+
+// Stepper Motors
+AccelStepper stepper1(1, 13, 12);
+AccelStepper stepper2(1, 11, 10);
+AccelStepper stepper3(1, 9, 8);
+AccelStepper stepper4(1, 7, 6);
+
+// Servo
+Servo servo_A0;
+
+// Homing Variables
+int Homing1 = 1;
+int Homing2 = 1;
+int Homing3 = 1;
 int HomingStepValue1 = 0;
 int HomingStepValue2 = 0;
 int HomingStepValue3 = 0;
-bool isAllowed = false; //Variable to allow code to continue once RFID is detected
+bool isAllowed = false;
 
-//lcd stuff
-#define I2C_ADDR    0x27
-#define LCD_COLUMNS 20
-#define LCD_LINES   4
-LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
+// Menu Items
+String menuItems[] = {
+  "Start Process",
+  "Check RFID",
+  "Servo Control",
+  "Stepper Control",
+  "Exit"
+};
+int menuSize = sizeof(menuItems) / sizeof(menuItems[0]);
 
-//RFID Code Portion
-#define SS_PIN 10
-#define RST_PIN 9
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Power On");
 
-//Led portion
-#define LED_PIN 7
-
-//rotary encoder stuff
-#define SW 43
-#define DT 45
-#define CLK 47
- int counter = 0; 
- int aState;
- int aLastState;  
-
-//stepper n servo stuff
-AccelStepper stepper1(1,13,12);
-AccelStepper stepper2(1,11,10);
-AccelStepper stepper3(1,9,8);
-AccelStepper stepper4(1,7,6);
-  Servo servo_A0;
-
-
-
-//start up code
- void setup() { 
-
-//serial monitor stuff
-     Serial.begin (9600);
-     Serial.println("Power On");
-
-
-//lcd stuff
-   lcd.init();
-   lcd.backlight();
+  // LCD Setup
+  lcd.init();
+  lcd.backlight();
   lcd.setCursor(5, 1);
   lcd.print("-POWER ON-");
-  lcd.setCursor(2, 1);
+  delay(1000);
 
-//variable stuff
-      Homing1 = 1;
-      Homing2 = 1;
-      Homing3 = 1;
-      HomingStepValue1 = 0;
-      HomingStepValue2 = 0;
-      HomingStepValue3 = 0;
+  // Stepper Motor Setup
+  stepper1.setMaxSpeed(1000);
+  stepper2.setMaxSpeed(1000);
+  stepper3.setMaxSpeed(1000);
+  stepper4.setMaxSpeed(1000);
 
-//pin modes
-   pinMode (A1,INPUT);
-   pinMode (5,INPUT);
-   pinMode (4,INPUT);
-   pinMode (3,INPUT);
-   pinMode (A0,OUTPUT);
-   pinMode (A2,OUTPUT);
-   pinMode (CLK,INPUT);
-   pinMode (DT,INPUT);
-   pinMode (SW,INPUT);
-   pinMode (LED_PIN, OUTPUT);
+  stepper1.setAcceleration(1000);
+  stepper2.setAcceleration(1000);
+  stepper3.setAcceleration(1000);
+  stepper4.setAcceleration(1000);
 
-//stepper values reset
-   stepper1.setMaxSpeed(1000);
-   stepper2.setMaxSpeed(1000);
-   stepper3.setMaxSpeed(1000);
-   stepper4.setMaxSpeed(1000);
+  // Rotary Encoder Setup
+  pinMode(ENCODER_CLK, INPUT);
+  pinMode(ENCODER_DT, INPUT);
+  pinMode(ENCODER_SW, INPUT_PULLUP);
+  lastStateCLK = digitalRead(ENCODER_CLK);
 
-   stepper1.setAcceleration(1000);
-   stepper2.setAcceleration(1000);
-   stepper3.setAcceleration(1000);
-   stepper4.setAcceleration(1000);
-
-   stepper1.setCurrentPosition(0);
-   stepper2.setCurrentPosition(0);
-   stepper3.setCurrentPosition(0);
-   stepper4.setCurrentPosition(0);
-
-
-//initial button check
-   if(digitalRead(2) == HIGH){
-      Serial.println("Warning: Button 1 Pressed Early");
-   }
-      if(digitalRead(3) == HIGH){
-      Serial.println("Warning: Button 2 Pressed Early");
-   }
-      if(digitalRead(4) == HIGH){
-      Serial.println("Warning: Button 3 Pressed Early");
-   }
-
-
-//servo reset
-  servo_A0.attach(A0, 500, 2500);
-        servo_A0.write(0);
-        delay(100);
-   Serial.println("Servo Reset");
-   delay(100);
-
-//more rotary encoder stuff
-   aLastState = digitalRead(CLK);   
-
-
-     Serial.println("Initialization Complete");     
-     delay(1000);
-
-  //RFID Portion
-  Serial.begin(9600);   // Initiate a serial communication
-  SPI.begin();      // Initiate  SPI bus
-  mfrc522.PCD_Init();   // Initiate MFRC522
+  // RFID Setup
+  SPI.begin();
+  mfrc522.PCD_Init();
   Serial.println("Approximate your card to the reader...");
-  Serial.println();
 
-// LED Stuff
-digitalWrite(LED_PIN, HIGH);  
+  // LED Setup
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  // Servo Setup
+  servo_A0.attach(A0, 500, 2500);
+  servo_A0.write(0);
+  Serial.println("Servo Reset");
+
+  // Display Initial Menu
+  updateMenuDisplay();
 }
 
+void loop() {
+  // Handle Rotary Encoder for Menu Navigation
+  handleRotaryEncoder();
 
-
-//main code
- void loop() { 
-
-  //RFID Portion
-// Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
-  {
-    return;
+  // Handle Menu Selection
+  if (digitalRead(ENCODER_SW) == LOW) {
+    if (!buttonPressed) {
+      selectMenuItem();
+      buttonPressed = true;
+    }
+  } else {
+    buttonPressed = false;
   }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
-  {
-    return;
+}
+
+// Function to handle rotary encoder
+void handleRotaryEncoder() {
+  int currentStateCLK = digitalRead(ENCODER_CLK);
+  if (currentStateCLK != lastStateCLK) {
+    if (digitalRead(ENCODER_DT) != currentStateCLK) {
+      menuIndex++;
+    } else {
+      menuIndex--;
+    }
+
+    if (menuIndex >= menuSize) menuIndex = 0;
+    if (menuIndex < 0) menuIndex = menuSize - 1;
+
+    updateMenuDisplay();
   }
-  //Show UID on serial monitor
-  Serial.print("UID tag :");
-  String content= "";
-  byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  lastStateCLK = currentStateCLK;
+}
+
+// Function to update the menu display
+void updateMenuDisplay() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Select an Option:");
+
+  for (int i = 0; i < 3; i++) {
+    int itemIndex = menuStartIndex + i;
+    if (itemIndex < menuSize) {
+      lcd.setCursor(1, i + 1);
+      if (itemIndex == menuIndex) {
+        lcd.print("> "); 
+      } else {
+        lcd.print("  "); 
+      }
+      lcd.print(menuItems[itemIndex]);
+    }
   }
-  Serial.println();
-  Serial.print("Message : ");
-  content.toUpperCase();
-  if (content.substring(1) == "87 E5 BE A7" || "EA FA 9C 82") //change here the UID of the card/cards that you want to give access
-  {
-    bool isAllowed = true; 
-    Serial.println("Authorized access");
-    Serial.println();
-    delay(3000);
+}
+
+// Function to execute selected menu item
+void selectMenuItem() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("You selected:");
+  lcd.setCursor(2, 2);
+  lcd.print(menuItems[menuIndex]);
+
+  delay(1500);  
+  if (menuIndex == 0) {
+    startProcess();
+  } else if (menuIndex == 1) {
+    checkRFID();
+  } else if (menuIndex == 2) {
+    servoControl();
+  } else if (menuIndex == 3) {
+    stepperControl();
   }
- 
- else   {
-    bool isAllowed = false; 
-    Serial.println(" Access denied");
-    delay(3000);
-  }
+  updateMenuDisplay();
+}
 
+// Function to start homing process
+void startProcess() {
+  lcd.clear();
+  lcd.print("Starting Process...");
+  Serial.println("Starting Process...");
 
-//rotary encoder stuff
-    aState = digitalRead(CLK); 
- 
-   if (aState != aLastState){     
-     if (digitalRead(DT) != aState) { 
-       counter ++;
-     } else {
-       counter --;
-     }
-     Serial.println(counter);
-   } 
-   aLastState = aState; 
+  // Homing Sequence
+  Homing1 = 1;
+  Homing2 = 1;
+  Homing3 = 1;
+  Serial.println("Full Homing Sequence Initiated");
 
+  // Display complete message
+  lcd.clear();
+  lcd.print("Homing Complete!");
+  Serial.println("Homing Complete!");
+  delay(2000);
+}
 
+// Function to check RFID
+void checkRFID() {
+  lcd.clear();
+  lcd.print("Scan RFID Now...");
+  Serial.println("Waiting for RFID...");
 
-//full homing sequence
-if (Homing1 != 0 && (Homing2 != 0 && Homing3 != 0)) {
+  while (true) {
+    if (!mfrc522.PICC_IsNewCardPresent()) continue;
+    if (!mfrc522.PICC_ReadCardSerial()) continue;
 
-//lcd stuff
+    String content = "";
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+      content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    }
+
+    content.toUpperCase();
     lcd.clear();
-    lcd.setCursor(6, 1);
-    lcd.print("-HOMING-");
-    lcd.setCursor(4, 3);
-    lcd.print("DO NOT TOUCH");
+    if (content == "87E5BEA7" || content == "EAFA9C82") {
+      lcd.print("Access Granted!");
+      Serial.println("Access Granted");
+      isAllowed = true;
+    } else {
+      lcd.print("Access Denied!");
+      Serial.println("Access Denied");
+      isAllowed = false;
+    }
+    delay(2000);
+    break;
+  }
+}
 
-//homing stepper 1
-     if (Homing1 != 0){
+// Function to control servo
+void servoControl() {
+  lcd.clear();
+  lcd.print("Moving Servo...");
+  Serial.println("Moving Servo...");
+  servo_A0.write(90);
+  delay(1000);
+  servo_A0.write(0);
+  Serial.println("Servo Reset");
+  lcd.print("Servo Reset!");
+  delay(1000);
+}
 
-  stepper1.setMaxSpeed(100.0);  
-  stepper1.setAcceleration(100.0); 
+// Function to control stepper motors
+void stepperControl() {
+  lcd.clear();
+  lcd.print("Stepper Test...");
+  Serial.println("Stepper Test...");
 
-   Serial.println("Stepper 1 is Homing");
-
-   HomingStepValue1 = 1;
-
-  while(digitalRead(2) != HIGH){  
-    stepper1.moveTo(HomingStepValue1);  
-    HomingStepValue1++;  
-    stepper1.run(); 
-    delay(5);
+  stepper1.moveTo(500);
+  while (stepper1.distanceToGo() != 0) {
+    stepper1.run();
+  }
+  delay(500);
+  stepper1.moveTo(0);
+  while (stepper1.distanceToGo() != 0) {
+    stepper1.run();
   }
 
-  stepper1.setCurrentPosition(0);  
-
-  stepper1.setMaxSpeed(100.0);      
-  stepper1.setAcceleration(100.0); 
-
-  HomingStepValue1 = -1;
-   Serial.println("Homing Switch 1 Hit");
-
-  stepper1.setCurrentPosition(0);
-  Serial.println("Homing 1 Completed");
-  
-     Homing1 = 0;
- }
-
-
-//homing stepper 2
-      if (Homing2 != 0){
-
-  stepper2.setMaxSpeed(100.0);  
-  stepper2.setAcceleration(100.0); 
-
-   Serial.println("Stepper 2 is Homing");
-
-   HomingStepValue2 = 1;
-
-  while(digitalRead(3) != HIGH){  
-    stepper2.moveTo(HomingStepValue2);  
-    HomingStepValue2++;  
-    stepper2.run(); 
-    delay(5);
-  }
-
-  stepper2.setCurrentPosition(0);  
-
-  stepper2.setMaxSpeed(100.0);      
-  stepper2.setAcceleration(100.0); 
-
-  HomingStepValue2 = -1;
-   Serial.println("Homing Switch 2 Hit");
-
-  stepper2.setCurrentPosition(0);
-  Serial.println("Homing 2 Completed");
-  
-     Homing2 = 0;
- }
-
-
-//homing stepper 3
-      if (Homing3 != 0){
-
-  stepper3.setMaxSpeed(100.0);  
-  stepper3.setAcceleration(100.0); 
-
-   Serial.println("Stepper 3 is Homing");
-
-   HomingStepValue3 = 1;
-
-  while(digitalRead(4) != HIGH){  
-    stepper3.moveTo(HomingStepValue3);  
-    HomingStepValue3++;  
-    stepper3.run(); 
-    delay(5);
-  }
-
-  stepper3.setCurrentPosition(0);  
-
-  stepper3.setMaxSpeed(100.0);      
-  stepper3.setAcceleration(100.0); 
-
-  HomingStepValue3 = -1;
-   Serial.println("Homing Switch 3 Hit");
-   
-  stepper3.setCurrentPosition(0);
-  Serial.println("Homing 3 Completed");
-  
-     Homing3 = 0;
-  }
-
-Serial.println("Full Homing Sequence Completed");
-
-//lcd stuff
-    lcd.clear();
-    lcd.setCursor(0, 1);
-    lcd.print(" -HOMING  COMPLETE-");
- }
-//write the rest of the stuff here
+  lcd.print("Stepper Reset!");
+  Serial.println("Stepper Reset!");
+  delay(1000);
 }
