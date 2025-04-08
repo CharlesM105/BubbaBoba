a75c861b1150e6824abdac0737be57e3c45070ef
Charlie <124851081+Charliee105@users.noreply.github.com>
2025-04-08 10:11:46 -0400

    Update _LED_Tester_Script.ino

--- _LED_Tester_Script/_LED_Tester_Script.ino
+++ _LED_Tester_Script/_LED_Tester_Script.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 2	 // input pin Neopixel is attached to
+#define PIN 5	 // input pin Neopixel is attached to
 
 #define NUMPIXELS      200// number of neopixels in strip
 
