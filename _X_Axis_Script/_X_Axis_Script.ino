2950e4ac81b67b765d22949cdc97b670fc188feb
Charlie <124851081+Charliee105@users.noreply.github.com>
2025-04-10 10:05:03 -0400

    Update _X_Axis_Script.ino

--- _X_Axis_Script/_X_Axis_Script.ino
+++ _X_Axis_Script/_X_Axis_Script.ino
@@ -5,27 +5,25 @@
 #define STEP_PIN 13
 #define EN_PIN 5
 
-AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);  // DRIVER = Step/Dir mode
-
-int stepsToMove = 100;
+AccelStepper stepper(1, 13, 12);  // DRIVER = Step/Dir mode
 
 void setup() {
   pinMode(EN_PIN, OUTPUT);
   digitalWrite(EN_PIN, LOW);  // Enable the driver
 
-  stepper.setMaxSpeed(1000);      // Max speed in steps/sec
-  stepper.setAcceleration(500);   // Acceleration in steps/sec^2
+  stepper.setMaxSpeed(10000);      // Max speed in steps/sec
+  stepper.setAcceleration(1000);   // Acceleration in steps/sec^2
 }
 
 void loop() {
   // Move 100 steps forward
-  stepper.moveTo(stepsToMove);
+  stepper.moveTo(100);
   stepper.runToPosition();  // Blocking
 
   delay(200);  // Optional pause
 
   // Move 100 steps backward
-  stepper.moveTo(-stepsToMove);
+  stepper.moveTo(-100);
   stepper.runToPosition();  // Blocking
 
   delay(200);  // Optional pause
