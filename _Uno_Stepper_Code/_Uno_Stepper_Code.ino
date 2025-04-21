c092898aae51fdb335c8346570682287a2c2bc57
Charlie <124851081+Charliee105@users.noreply.github.com>
2025-04-21 09:59:19 -0400

    Update _Uno_Stepper_Code.ino

--- _Uno_Stepper_Code/_Uno_Stepper_Code.ino
+++ _Uno_Stepper_Code/_Uno_Stepper_Code.ino
@@ -1,9 +1,5 @@
-//Homing Code :)
-
-//libaries
 #include <AccelStepper.h>
 
-//variables
 int Homing1 = 0;
 int Homing2 = 0;
 int Homing3 = 0;
@@ -11,21 +7,17 @@ int HomingStepValue1 = 0;
 int HomingStepValue2 = 0;
 int HomingStepValue3 = 0;
 
-//stepper stuff
 AccelStepper stepper1(1,13,12);
-AccelStepper stepper2(1,9,8);
-AccelStepper stepper3(1,11,10);
+AccelStepper stepper2(1,11,10);
+AccelStepper stepper3(1,9,8);
 AccelStepper stepper4(1,7,6);
 
-//start up code
 
- void setup() { 
+void setup() {
 
-//serial monitor stuff
-     Serial.begin (115200);
+       Serial.begin (9600);
      Serial.println("Power On");
-
-//variable stuff
+     
       Homing1 = 1;
       Homing2 = 1;
       Homing3 = 1;
@@ -33,16 +25,10 @@ AccelStepper stepper4(1,7,6);
       HomingStepValue2 = 0;
       HomingStepValue3 = 0;
 
-//pin modes
-   pinMode (4,INPUT);
-   pinMode (3,INPUT);
-   pinMode (2,INPUT);
-
-//stepper values reset
-   stepper1.setMaxSpeed(3000);
-   stepper2.setMaxSpeed(3000);
-   stepper3.setMaxSpeed(3000);
-   stepper4.setMaxSpeed(3000);
+   stepper1.setMaxSpeed(1000);
+   stepper2.setMaxSpeed(1000);
+   stepper3.setMaxSpeed(1000);
+   stepper4.setMaxSpeed(1000);
 
    stepper1.setAcceleration(1000);
    stepper2.setAcceleration(1000);
@@ -54,29 +40,16 @@ AccelStepper stepper4(1,7,6);
    stepper3.setCurrentPosition(0);
    stepper4.setCurrentPosition(0);
 
-//initial button check
-   if(digitalRead(2) == HIGH){
-      Serial.println("Warning: Button 1 Pressed Early");
-   }
-      if(digitalRead(3) == HIGH){
-      Serial.println("Warning: Button 2 Pressed Early");
-   }
-      if(digitalRead(4) == HIGH){
-      Serial.println("Warning: Button 3 Pressed Early");
-   }
- }
+}
 
-//main code
- void loop() { 
+void loop() {
+ if (Homing1 != 0 && (Homing2 != 0 && Homing3 != 0)) {
 
-//full homing sequence
-if (Homing1 != 0 && (Homing2 != 0 && Homing3 != 0)) {
-
-//homing stepper 1
+  //homing stepper 1
      if (Homing1 != 0){
 
-  stepper1.setMaxSpeed(5000.0);  
-  stepper1.setAcceleration(1000.0); 
+  stepper1.setMaxSpeed(3000.0);  
+  stepper1.setAcceleration(3000.0); 
 
    Serial.println("Stepper 1 is Homing");
 
@@ -86,13 +59,13 @@ if (Homing1 != 0 && (Homing2 != 0 && Homing3 != 0)) {
     stepper1.moveTo(HomingStepValue1);  
     HomingStepValue1++;  
     stepper1.run(); 
-    delay(5);
+    delay(2);
   }
 
   stepper1.setCurrentPosition(0);  
 
-  stepper1.setMaxSpeed(5000.0);      
-  stepper1.setAcceleration(1000.0); 
+  stepper1.setMaxSpeed(3000);      
+  stepper1.setAcceleration(3000.0); 
 
   HomingStepValue1 = -1;
    Serial.println("Homing Switch 1 Hit");
@@ -107,24 +80,24 @@ if (Homing1 != 0 && (Homing2 != 0 && Homing3 != 0)) {
 //homing stepper 2
       if (Homing2 != 0){
 
-  stepper4.setMaxSpeed(5000.0);  
-  stepper4.setAcceleration(1000.0); 
+  stepper4.setMaxSpeed(3000.0);  
+  stepper4.setAcceleration(3000.0); 
 
    Serial.println("Stepper 2 is Homing");
 
-   HomingStepValue2 = 1;
+   HomingStepValue2 = -1;
 
   while(digitalRead(2) != HIGH){  
     stepper4.moveTo(HomingStepValue2);  
     HomingStepValue2--;  
     stepper4.run(); 
-    delay(5);
+    delay(2);
   }
 
   stepper4.setCurrentPosition(0);  
 
-  stepper4.setMaxSpeed(5000.0);      
-  stepper4.setAcceleration(1000.0); 
+  stepper4.setMaxSpeed(3000.0);      
+  stepper4.setAcceleration(3000.0); 
 
   HomingStepValue2 = -1;
    Serial.println("Homing Switch 2 Hit");
@@ -139,34 +112,35 @@ if (Homing1 != 0 && (Homing2 != 0 && Homing3 != 0)) {
 //homing stepper 3
       if (Homing3 != 0){
 
-  stepper3.setMaxSpeed(20000.0);  
-  stepper3.setAcceleration(1000.0); 
+  stepper2.setMaxSpeed(3000.0);  
+  stepper2.setAcceleration(3000.0); 
 
    Serial.println("Stepper 3 is Homing");
 
    HomingStepValue3 = -1;
 
   while(digitalRead(3) != HIGH){  
-    stepper3.moveTo(HomingStepValue3);  
+    stepper2.moveTo(HomingStepValue3);  
     HomingStepValue3--;  
-    stepper3.run(); 
+    stepper2.run(); 
     delay(2);
   }
 
-  stepper3.setCurrentPosition(0);  
+  stepper2.setCurrentPosition(0);  
 
-  stepper3.setMaxSpeed(20000.0);      
-  stepper3.setAcceleration(1000.0); 
+  stepper2.setMaxSpeed(3000.0);      
+  stepper2.setAcceleration(3000.0); 
 
-  HomingStepValue3 = +1;
+  HomingStepValue3 = -1;
    Serial.println("Homing Switch 3 Hit");
    
-  stepper3.setCurrentPosition(0);
+  stepper2.setCurrentPosition(0);
   Serial.println("Homing 3 Completed");
   
      Homing3 = 0;
   }
 
-Serial.println("Full Homing Sequence Completed"); 
+Serial.println("Full Homing Sequence Completed");
+
  }
 }
