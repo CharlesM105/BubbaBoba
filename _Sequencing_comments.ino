5c4e0cfc1ccb41f8a91ca408967877c54ace1337
Vincent Larson <Bumba015@yahoo.com>
2025-04-26 19:25:17 -0400

    Add files via upload

---
+++ _Sequencing_comments.ino
@@ -0,0 +1,30 @@
+
+
+//all systems reset
+//x, y, z motors home
+//customer swipes rfid card
+//customer selects flavor
+//customer selects flavor to milk ratio
+//customer selects if they want boba or not
+//customer presses start
+//Stepper x move to have nozzle over cup
+//stepper y move middle
+//stepper z move down
+//servo for cover rotate
+//enable spinny eyes
+//dispense milk and flavor 
+//cover tubes again/disable spinny eyes
+//stepper z move up
+//stepper x move to have mixer over cup
+//stepper z move down
+//enable relay for mixer and spinny eyes
+//disable relay for mixer and spinny eyes
+//stepper z move up
+//stepper y home
+//enable stepper for boba dispenser
+//enable relay for boba dispenser shaker
+//disable stepper for boba dispenser
+//disable relay for boba dispenser shaker
+//stepper y move to front
+//wait 10 seconds
+//home x, y, z motors
